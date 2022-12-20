bb102dff4723c5d26113cf7c388f1a79454d38ef
MagnoDev <magno-god64@outlook.com>
2022-12-20 03:39:34 -0600

    Creacion de navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/Inicio';
+import Proyectos from './pages/Proyectos';
+import Experiencia from './pages/Experiencia';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {}
+      <Router>
+        <Navbar/>
+          <Routes>
+
+            <Route path="/" element={<Inicio />} />
+            <Route path="/proyectos" element={<Proyectos />} />
+            <Route path="/experiencia" element={<Experiencia />} />
+
+          </Routes>
+        
+      </Router>
     </div>
   );
 }
